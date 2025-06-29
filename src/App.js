e58ec9ae20ef92915ca8d5b8ebd8a383bc4f5c66
Richard Wasonga <rowwasonga@gmail.com>
2025-06-29 04:01:31 +0300

    final update

--- src/App.js
+++ src/App.js
@@ -1,9 +1,5 @@
 import React from 'react';
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from 'react-router-dom';
-
+import {createBrowserRouter,RouterProvider,} from 'react-router-dom';
 import Layout from './components/Layout';
 import Home from './components/Home';
 import LoginForm from './components/LoginForm';
--- src/components/ErrorPage.js
+++ src/components/ErrorPage.js
@@ -3,10 +3,10 @@ import { Link } from 'react-router-dom';
 
 function ErrorPage() {
   return (
-    <div className="p-8 text-center">
-      <h1 className="text-4xl font-bold">404 - Page Not Found</h1>
-      <p className="mt-4">The page you’re looking for doesn’t exist.</p>
-      <Link to="/" className="text-blue-500 underline mt-6 inline-block">Go to Home</Link>
+    <div className="error-page">
+      <h1 className="error-title">404 - Page Not Found</h1>
+      <p className="error-message">The page you’re looking for doesn’t exist.</p>
+      <Link to="/" className="home-link">Go to Home</Link>
     </div>
   );
 }
